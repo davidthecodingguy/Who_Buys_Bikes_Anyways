a8fc0276e9b1c0c4c8673e61ec756abe5c8d6928
David York <davidyork1995@gmail.com>
2023-06-28 14:08:32 -0400

    Started merging dfs, began fixing empty df issue

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-columnsbikebuyers = ["ID","Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
-columnsbikebuyersfake = ["First Name", "Last Name", "Birthday", "Country", "City"]
-df = pd.read_csv("assets/bike_buyers.csv", usecols=columnsbikebuyers)
-df2 =pd. read_csv("assets/bike_buyers_fake_details.csv", usecols=columnsbikebuyersfake)
-print(df, df2)
+columnsbikebuyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
+columnsbikebuyersfakedata = ["First Name", "Last Name", "Birthday", "Country", "City"]
+df1 = pd.read_csv("assets/bike_buyers.csv", usecols=None)
+df2 =pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=None)
+
+#currently says it's an empty data frame. Could have to do with usecols?
+mergeddf = df1.merge(df2, left_on="Marital Status", right_on="First Name")
+print(mergeddf)
