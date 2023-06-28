58ec4f7b9d340e28f6def936a6635b2c6b9cb775
David York <davidyork1995@gmail.com>
2023-06-28 10:51:09 -0400

    Added file calls to dataframes

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+columnsbikebuyers = ["ID","Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
+columnsbikebuyersfake = ["First Name", "Last Name", "Birthday", "Country", "City"]
+df = pd.read_csv("assets/bike_buyers.csv", usecols=columnsbikebuyers)
+df2 =pd. read_csv("assets/bike_buyers_fake_details.csv", usecols=columnsbikebuyersfake)
+print(df, df2)
