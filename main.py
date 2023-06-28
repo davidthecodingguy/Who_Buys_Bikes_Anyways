9ad1ab4a518972bc155a616585015af168e17d22
David York <davidyork1995@gmail.com>
2023-06-28 15:41:54 -0400

    Restructured merge, began fixing error

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import seaborn as sns
 import os
 
 columnsbikebuyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
-columnsbikebuyersfakedata = ["First Name", "Last Name", "Birthday", "Country", "City"]
-df1 = pd.read_csv("assets/bike_buyers.csv", usecols=None)
-df2 =pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=None)
-
-#currently says it's an empty data frame. Could have to do with usecols?
-mergeddf = df1.merge(df2, left_on="Marital Status", right_on="First Name")
-print(mergeddf)
+columnsbikebuyersfakedata = ["Birthday", "Country", "City", "Marital Status"]
+df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columnsbikebuyers)
+df2 =pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columnsbikebuyersfakedata)
+#need to change integers to strings in Marital Status column. Might fix error. Python advises to use pd.concat. This also gives an error
+merged_df = pd.merge(df1, df2)
+#merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
+print("df1", "df2", merged_df)
