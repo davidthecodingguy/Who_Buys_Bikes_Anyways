d1417fb4ff59582db78dcb20636e98a2c3dfcb50
David York <davidyork1995@gmail.com>
2023-07-03 15:52:37 -0400

    Deleted unneeded code, added comments

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+#reading in two data files. Feature #1
 columnsbikebuyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
 columnsbikebuyersfakedata = ["Birthday", "Country", "City", "Marital Status"]
 df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columnsbikebuyers)
 df2 =pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columnsbikebuyersfakedata)
-#need to change integers to strings in Marital Status column. Might fix error. Python advises to use pd.concat. This also gives an error
-#merged_df = pd.merge(df1, df2)
-#merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
 
+#clean and Pandas merge. Feature #2
 df1.dropna(inplace=True)
 df2.replace(to_replace=1.0, value="Married", inplace=True)
 df2.fillna("Single", inplace=True)
+merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
 
-print(df1, df2)
+#TESTING FUNCTIONALITY, WILL DELETE LATER
+print(merged_df)
