c60f4ec901face7e7bde153f8167cda7210c6e43
David York <davidyork1995@gmail.com>
2023-07-08 15:37:02 -0400

    Added mulitple pivot tables

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ columns_bike_buyers = ["Marital Status", "Gender", "Income", "Children", "Educat
 columns_bike_buyers_fake_data = ["Birthday", "Country", "City", "Marital Status"]
 df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columns_bike_buyers)
 df2 = pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columns_bike_buyers_fake_data)
-
 #clean and Pandas merge. Feature #2
 df1.dropna(inplace=True)
 df2.replace(to_replace=1.0, value="Married", inplace=True)
 df2.fillna("Single", inplace=True)
 merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
 
-occupational_pivot_table = pd.pivot_table(merged_df, index=["Occupation", "Purchased Bike", "Education"], aggfunc={"Income": np.mean, "Age": np.mean})
+#pivot tables. Part of Feature #3
+job_pt = pd.pivot_table(merged_df, index=["Occupation", "Purchased Bike"], aggfunc={"Income": [max, np.mean, min]})
+commute_pt = pd.pivot_table(merged_df, index=[ "Commute Distance", "Purchased Bike"], aggfunc={"Age": [max, np.mean, min], "Cars": [max, min]})
+education_pt = pd.pivot_table(merged_df, index=["Education", "Purchased Bike"], aggfunc={"Age": [max, np.mean, min]})
+marital_pt = pd.pivot_table(merged_df, index=["Purchased Bike", "Marital Status"], aggfunc={"Age": [max, np.mean, min]})
+country_pt = pd.pivot_table(merged_df, index=["Purchased Bike", "Country"], aggfunc={"Age": [max, np.mean, min]})
+
 #TESTING FUNCTIONALITY, WILL DELETE LATER
-print(occupational_pivot_table)
+print(job_pt)
+print(commute_pt)
+print(education_pt)
+print(marital_pt)
+print(country_pt)
 #print(merged_df)
