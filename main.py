15b61d579a5666e26ccb6119329dac231abef535
David York <davidyork1995@gmail.com>
2023-07-08 14:22:12 -0400

    Added pivot tables for averaging values

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
 #reading in two data files. Feature #1
-columnsbikebuyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
-columnsbikebuyersfakedata = ["Birthday", "Country", "City", "Marital Status"]
-df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columnsbikebuyers)
-df2 =pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columnsbikebuyersfakedata)
+columns_bike_buyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
+columns_bike_buyers_fake_data = ["Birthday", "Country", "City", "Marital Status"]
+df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columns_bike_buyers)
+df2 = pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columns_bike_buyers_fake_data)
 
 #clean and Pandas merge. Feature #2
 df1.dropna(inplace=True)
@@ -15,5 +16,7 @@ df2.replace(to_replace=1.0, value="Married", inplace=True)
 df2.fillna("Single", inplace=True)
 merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
 
+pivot_table = pd.pivot_table(merged_df, index="Marital Status", aggfunc={"Income": np.mean, "Age": np.mean})
 #TESTING FUNCTIONALITY, WILL DELETE LATER
-print(merged_df)
+print(pivot_table)
+#print(merged_df)
