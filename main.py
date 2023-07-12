0ca00a6d773f5850c8fcd60fc2e24a3f01b72e61
David York <davidyork1995@gmail.com>
2023-07-11 20:20:09 -0400

    Added bar plots and views

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import os
 
 #Feature #1: Reading in two data files
 columns_bike_buyers = ["Marital Status", "Gender", "Income", "Children", "Education", "Occupation", "Home Owner", "Cars", "Commute Distance", "Region", "Age", "Purchased Bike"]
@@ -11,29 +9,18 @@ df1 = pd.read_csv("assets/bike_buyers.csv", usecols=columns_bike_buyers)
 df2 = pd.read_csv("assets/bike_buyers_fake_details.csv", usecols=columns_bike_buyers_fake_data)
 
 #Feature #2: Clean data and merge data dataframes using Pandas
+df1.replace(to_replace="No", value=pd.np.nan, inplace=True)
 df1.dropna(inplace=True)
 df2.replace(to_replace=1.0, value="Married", inplace=True)
 df2.fillna("Single", inplace=True)
 merged_df = df1.merge(df2, left_on="Marital Status", right_on="Marital Status")
 
-#Part of Feature #3: Creating pivot tables
-job_pt = pd.pivot_table(merged_df, index=["Occupation", "Purchased Bike"], aggfunc={"Income": [max, np.mean, min]})
-commute_pt = pd.pivot_table(merged_df, index=[ "Commute Distance", "Purchased Bike"], aggfunc={"Age": [max, np.mean, min], "Cars": [max, min]})
-education_pt = pd.pivot_table(merged_df, index=["Education", "Purchased Bike"], aggfunc={"Age": [max, np.mean, min]})
-marital_pt = pd.pivot_table(merged_df, index=["Purchased Bike", "Marital Status"], aggfunc={"Age": [max, np.mean, min]})
-country_pt = pd.pivot_table(merged_df, index=["Purchased Bike", "Country"], aggfunc={"Age": [max, np.mean, min]})
+#Feature #3: Creating pivot tables and visualizing data with bar graphs
+job_pt = pd.pivot_table(merged_df, index=["Occupation"], aggfunc={"Income": [max, np.mean, min]}).plot.barh(figsize=(10,7), title="Bike Purchaser Occupation Types and their Incomes")
+commute_pt = pd.pivot_table(merged_df, index=[ "Commute Distance"], aggfunc={"Age": [max, np.mean, min], "Cars": [max, min]}).plot.barh(figsize=(10,7), title="Bike Purchaser Ages and their Commute Distances")
+education_pt = pd.pivot_table(merged_df, index=["Education"], aggfunc={"Income": [max, np.mean, min]}).plot.barh(figsize=(10,7), title="Bike Purchaser Education Levels and their Incomes")
+marital_pt = pd.pivot_table(merged_df, index=["Marital Status"], aggfunc={"Age": [max, np.mean, min]}).plot.barh(figsize=(10,7), title="Bike Purchaser Marital Status and their Ages")
 
-#x = merged_df["Occupation"]
-#y = merged_df["Income"]
-
-#plt.rcParams["figure.autolayout"] = True
-#fig, ax1 = plt.subplots()
-#ax1.bar(x, y, color="green")
-
-#plt.show()
-#TESTING FUNCTIONALITY, WILL DELETE LATER
-#print(job_pt)
-#print(commute_pt)
-#print(education_pt)
-#print(marital_pt)
-#print(country_pt)
+#Data visualization allows users to click through tabs to see different data views
+plt.rcParams["figure.autolayout"] = True
+plt.show()
